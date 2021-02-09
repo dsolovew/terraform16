08b1614112f3567a09e258f22fbad18f5ba03ef5
dsolovew <dsolovew@gmail.com>
2021-02-09 23:41:41 +0300

    name

--- ami.tf
+++ ami.tf
@@ -15,7 +15,8 @@ data "aws_ami" "latest_amazon_linux" {
     owners = [ "amazon" ]
     most_recent = true
     filter {
-        value = ["amzn2-ami-hvm-*-x86_64-gp2"]
+        name = "name"
+        values = ["amzn2-ami-hvm-*-x86_64-gp2"]
     }
 }
 
