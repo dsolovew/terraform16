44ee9def003157f7c540ae78fde5dab3956f7043
dsolovew <dsolovew@gmail.com>
2021-02-09 23:35:44 +0300

    amazon ami

--- ami.tf
+++ ami.tf
@@ -2,12 +2,20 @@ provider "aws" {
   region = "us-east-2"
 }
 
-data "aws_ami" "latest_ubuntu"{
+data "aws_ami" "latest_ubuntu" { 
     owners = [ "099720109477" ]
     most_recent = true
     filter {
         name = "name"
         values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+    }
+}
+
+data "aws_ami" "latest_amazon_linux" {  
+    owners = [ "amazon" ]
+    most_recent = true
+    filter {
+        value = ["amzn2-ami-hvm-*-x86_64-gp2"]
     }
 }
 
@@ -17,4 +25,12 @@ output "latest_ubuntu_ami_id" {
 
 output "latest_ubuntu_ami_name" {
   value = data.aws_ami.latest_ubuntu.name
+}
+
+output "latest_ubuntu_ami_id" {
+  value = data.aws_ami.latest_amazon_linux.id
+}
+
+output "latest_ubuntu_ami_name" {
+  value = data.aws_ami.latest_amazon_linux.name
 }
